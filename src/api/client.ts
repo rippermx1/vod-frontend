1b5e71c4e81b8bdb1b5326055d28c814250ac80c
Carlos Valdes <valdescarlos17@gmail.com>
2026-01-09 13:43:58 -0300

    url api

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // MVP: Hardcoded URL or ENV
-const API_URL = 'http://127.0.0.1:8000/api/v1';
+const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000/api/v1';
 
 export const api = axios.create({
     baseURL: API_URL,
